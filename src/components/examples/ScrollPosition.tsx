aa8e4cdd14885799e9c2db69f173bd2cb67641bc
seoulcircle <dlghd1997@naver.com>
2025-04-28 00:42:08 +0900

    useRef 연습 컴포넌트 3개 추가

---
+++ src/components/examples/ScrollPosition.tsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+export default function ScrollPosition() {
+  const [position, setPosition] = useState<number>(0);
+
+  const onScroll = () => {
+    console.log(position);
+    const scrollTop = window.scrollY;
+    const scrollHeight = document.documentElement.scrollHeight;
+    const clientHeight = window.innerHeight;
+    const scrollPercent = (scrollTop / (scrollHeight - clientHeight)) * 100;
+    setPosition(scrollPercent);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+
+    return () => {
+      window.removeEventListener("scroll", onScroll); // 이벤트 제거 (메모리 누수 방지)
+    };
+  }, []);
+  return (
+    <>
+      <div style={{ height: "200vh", padding: "20px" }}>
+        <p style={{ position: "fixed" }}>
+          현재 스크롤 포지션은 {Math.round(position)} % 입니다.
+        </p>
+      </div>
+    </>
+  );
+}
